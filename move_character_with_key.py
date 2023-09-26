13a5d6e96062e245b165d67f4b60d854f0044d2e
Jung Geuk Hun <111038432+rmrgns@users.noreply.github.com>
2023-09-26 13:08:08 +0900

    Update move_character_with_key.py
    화면 조정 및 화면 밖을 벗어나지 않도록 코드 수정

--- move_character_with_key.py
+++ move_character_with_key.py
@@ -1,8 +1,8 @@
 from pico2d import *
 
-
-open_canvas()
-grass = load_image('grass.png')
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+tuk_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 
 
@@ -16,30 +16,53 @@ def handle_events():
             running = False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
-                dir += 1
+                dir[0] += 1
             elif event.key == SDLK_LEFT:
-                dir -= 1
+                dir[0] -= 1
+            elif event.key == SDLK_UP:
+                dir[1] += 1
+            elif event.key == SDLK_DOWN:
+                dir[1] -= 1
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
-                dir -= 1
+                dir[0] -= 1
             elif event.key == SDLK_LEFT:
-                dir += 1
+                dir[0] += 1
+            elif event.key == SDLK_UP:
+                dir[1] -= 1
+            elif event.key == SDLK_DOWN:
+                dir[1] += 1
+
+def checkScreenOut():
+    global x, y
+    if x > TUK_WIDTH:
+        x = TUK_WIDTH
+    elif x < 0:
+        x = 0
+    if y > TUK_HEIGHT:
+        y = TUK_HEIGHT
+    elif y < 0:
+        y = 0
+
 
 running = True
-x = 800 // 2
+x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 frame = 0
-dir = 0
+dir = [0, 0]
+hide_cursor()
 
 while running:
     clear_canvas()
-    grass.draw(400, 30)
-    character.clip_draw(frame*100, 100, 100, 100, x, 90)
+    tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    character.clip_draw(frame*100, 100, 100, 100, x, y)
     update_canvas()
     handle_events()
     frame = (frame+1) % 8
-    x += dir * 5
+    x += dir[0] * 20
+    y += dir[1] * 20
+    checkScreenOut()
     delay(0.05)
 
 
